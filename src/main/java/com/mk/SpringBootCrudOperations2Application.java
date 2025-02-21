8b1ba03b29934e128302eae75f2c57ff081b83b0
madhukrishnaonline <mk9032605@gmail.com>
2025-02-21 15:05:40 +0530

    latest Commit

---
+++ src/main/java/com/mk/SpringBootCrudOperations2Application.java
@@ -0,0 +1,12 @@
+package com.mk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudOperations2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudOperations2Application.class, args);
+	}
+}
